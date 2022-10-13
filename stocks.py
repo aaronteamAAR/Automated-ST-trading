f9b270fd47fb6fd174d218be35045f0c074af586
Aaron Chris <rooneychris83@gmail.com>
2022-10-13 11:15:57 -0700

    Debugging

--- stocks.py
+++ stocks.py
@@ -1,8 +1,25 @@
 import yfinance as yf
 import pandas as pd
+import config
 import schedule
 import time
+import robin_stocks.robinhood as rh
 
+
+
+def login(days):
+    time_logged_in = 60*60*24*days
+    rh.authentication.login(username=config.USERNAME,
+                            password=config.PASSWORD,
+                            expiresIn=time_logged_in,
+                            scope='internal',
+                            by_sms=True,
+                            store_session=True)
+
+
+# This session handles the logout 
+def logout():
+    rh.authentication.logout()
 
         
 def get_price(symbol):
@@ -11,17 +28,16 @@ def get_price(symbol):
     return(week_data)
 
 
-            
-def get_priceNow():
-    tag = get_price('AAPL')
-    print(tag) 
+
+def get_tag():
+    market_tag = rh.markets.get_top_100(info=None)
+    for x in range(len(market_tag)):
+      syb = market_tag[x]["symbol"]
+      print(syb)
+
     
-    
-            
 if __name__ == "__main__":
-    schedule.every(5).seconds.do(get_priceNow)
-  
-while True:
-    schedule.run_pending()
-    time.sleep(1)
-    
+    login(days=1)
+    tops = get_tag()
+    stocks = list[tops]
+    print(stocks)
