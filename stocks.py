2fdd2a9ef26756c6640fa2352f2d0104ae81c157
Aaron Chris <rooneychris83@gmail.com>
2022-10-13 00:12:46 -0700

    Added timer to price function

--- stocks.py
+++ stocks.py
@@ -1,44 +1,27 @@
 import yfinance as yf
 import pandas as pd
+import schedule
+import time
 
 
+        
+def get_price(symbol):
+    ticker = yf.Ticker(symbol)
+    week_data = ticker.history(interval="1m",period='max')
+    return(week_data)
 
 
-
-def print_info(yf_tickers):
-    if isinstance(yf_tickers,yf.Ticker):
-        print(f"\n{'='*80}")
-        space = ' '
-        print(f"{space*33}{yf_tickers.info['symbol']}\n")
-        for key in yf_tickers.info:
-            print(f"--> {key:>29} : {yf_tickers.info[key]}")
             
-    elif isinstance(yf_tickers,yf.Tickers):
-        for ticker in yf_tickers.tickers:
-            print(f"\n{'='*80}")
-            space = ' '
-            print(f"{space*33}{ticker.info['symbol']}\n")
-            for key in ticker.info.keys():
-                print(f"--> {key:>29} : {ticker.info[key]}")
-                
-def print_table(yf_tickers):
-    if isinstance(yf_tickers,yf.Ticker):
-        ticker = yf_tickers
-        print(f"| {ticker.info.get('symbol','NONE'):<5} | {ticker.info.get('sector','NONE'):>25} | " + \
-              f"{ticker.info.get('currency','NONE'):>4} | {ticker.info.get('quoteType','NONE'):>6} | " + \
-              f"{ticker.info.get('shortName','NONE'):<35} |")
-
-    elif isinstance(yf_tickers,yf.Tickers):
-        for ticker in yf_tickers.tickers:
-            print(f"| {ticker.info.get('symbol','NONE'):<5} | {ticker.info.get('sector','NONE'):>25} | " + \
-                  f"{ticker.info.get('currency','NONE'):>4} | {ticker.info.get('quoteType','NONE'):>6} | " + \
-                  f"{ticker.info.get('shortName','NONE'):<35} |")
-            
-            
-            
-            
+def get_priceNow():
+    tag = get_price('AAPL')
+    print(tag) 
+    
+    
             
 if __name__ == "__main__":
-    ticker = yf.Ticker('AAPL')
-    print(ticker.history(interval="1m"))
+    schedule.every(5).seconds.do(get_priceNow)
+  
+while True:
+    schedule.run_pending()
+    time.sleep(1)
     
