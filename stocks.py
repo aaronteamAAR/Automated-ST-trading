b90f1d7fde7c0616d7f498d2ce9cbe061a827af6
Aaron Chris <rooneychris83@gmail.com>
2022-10-15 16:07:27 -0700

    Still working

--- stocks.py
+++ stocks.py
@@ -2,20 +2,23 @@ import yfinance as yf
 import pandas as pd
 import schedule
 import time
+import datetime
 import trader
         
 def get_price(symbol):
-    ticker = yf.Ticker(symbol)
+    ticker = yf.Ticker(symbol).get
     week_data = ticker.history(interval="1m",period='max')
     return(week_data)
             
 
 
 
-msft = yf.Ticker('TSLA', 'AAPL', 'AMZN', 'F', 'AMC', 'SNDL', 'MSFT', 'DIS', 'NIO', 
+ticks = ('TSLA', 'AAPL', 'AMZN', 'F', 'AMC', 'SNDL', 'MSFT', 'DIS', 'NIO', 
 'META', 'NFLX', 'NVDA', 'LCID', 'TWTR', 'VOO', 'SNAP', 'SPY', 'PFE', 'GPRO', 'GOOGL', 'AAL', 'PLUG', 'CCL', 'BABA', 'HOOD', 'BAC', 'SBUX', 'RIVN', 'PLTR', 'DAL', 'AMD', 'NOK', 'GME', 'TLRY', 'KO', 'COIN', 'VTI', 'T', 'CGC', 'SPCE', 'PYPL', 'UBER', 'MRNA', 'BB', 'GM', 'FCEL', 'RBLX', 'GE', 'WMT', 'SQ', 'PSEC', 'NCLH', 'WKHS', 'BA', 'DKNG', 'ABNB', 'QQQ', 'CRON', 'UAL', 'SIRI', 'CHPT', 'NKLA', 'JNJ', 'NKE', 'XOM', 'LUV', 'SOFI', 'INTC', 'ARKK', 'RIOT', 'MRO', 'PTON', 'DWAC', 'GOOG', 'OCGN', 'SHOP', 'JBLU', 'COST', 'ET', 'BNGO', 'RCL', 'BRK.B', 'SONY', 'TSM', 'WISH', 'RYCEY', 'V', 'JPM', 'CPRX', 
 'TGT', 'CLOV', 'ZM', 'FUBO', 'O', 'RITM', 'CRM', 'IVR', 'QS', 'SPHD', 'PENN')
 
-stockingo = msft.history(interval='1m',period='1d')
-print(stockingo)
-    
+for x in ticks:
+      toStr = str(x)
+      syb = yf.Ticker(toStr)
+      data = pd.DataFrame(syb.history(interval="1m",period='1d',))
+      print(toStr, data['Open'])
