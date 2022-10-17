aa659e6ad6ffb17036c6271fe690eb5568e6bf9a
Aaron Chris <rooneychris83@gmail.com>
2022-10-16 22:50:41 -0700

    Added jupyter

--- stocks.py
+++ stocks.py
@@ -9,9 +9,7 @@ def get_price(symbol):
     ticker = yf.Ticker(symbol).get
     week_data = ticker.history(interval="1m",period='max')
     return(week_data)
-            
-
-
+    
 
 ticks = ('TSLA', 'AAPL', 'AMZN', 'F', 'AMC', 'SNDL', 'MSFT', 'DIS', 'NIO', 
 'META', 'NFLX', 'NVDA', 'LCID', 'TWTR', 'VOO', 'SNAP', 'SPY', 'PFE', 'GPRO', 'GOOGL', 'AAL', 'PLUG', 'CCL', 'BABA', 'HOOD', 'BAC', 'SBUX', 'RIVN', 'PLTR', 'DAL', 'AMD', 'NOK', 'GME', 'TLRY', 'KO', 'COIN', 'VTI', 'T', 'CGC', 'SPCE', 'PYPL', 'UBER', 'MRNA', 'BB', 'GM', 'FCEL', 'RBLX', 'GE', 'WMT', 'SQ', 'PSEC', 'NCLH', 'WKHS', 'BA', 'DKNG', 'ABNB', 'QQQ', 'CRON', 'UAL', 'SIRI', 'CHPT', 'NKLA', 'JNJ', 'NKE', 'XOM', 'LUV', 'SOFI', 'INTC', 'ARKK', 'RIOT', 'MRO', 'PTON', 'DWAC', 'GOOG', 'OCGN', 'SHOP', 'JBLU', 'COST', 'ET', 'BNGO', 'RCL', 'BRK.B', 'SONY', 'TSM', 'WISH', 'RYCEY', 'V', 'JPM', 'CPRX', 
@@ -22,3 +20,5 @@ for x in ticks:
       syb = yf.Ticker(toStr)
       data = pd.DataFrame(syb.history(interval="1m",period='1d',))
       print(toStr, data['Open'])
+      
+      
