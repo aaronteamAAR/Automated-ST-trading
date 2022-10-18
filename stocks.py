b922dd7328eee5c04bdec01d2de3593b742325e7
Aaron Chris <rooneychris83@gmail.com>
2022-10-17 21:43:04 -0700

    Made progress

--- stocks.py
+++ stocks.py
@@ -15,12 +15,20 @@ ticks = ('TSLA', 'AAPL', 'AMZN', 'F', 'AMC', 'SNDL', 'MSFT', 'DIS', 'NIO',
 'META', 'NFLX', 'NVDA', 'LCID', 'TWTR', 'VOO', 'SNAP', 'SPY', 'PFE', 'GPRO', 'GOOGL', 'AAL', 'PLUG', 'CCL', 'BABA', 'HOOD', 'BAC', 'SBUX', 'RIVN', 'PLTR', 'DAL', 'AMD', 'NOK', 'GME', 'TLRY', 'KO', 'COIN', 'VTI', 'T', 'CGC', 'SPCE', 'PYPL', 'UBER', 'MRNA', 'BB', 'GM', 'FCEL', 'RBLX', 'GE', 'WMT', 'SQ', 'PSEC', 'NCLH', 'WKHS', 'BA', 'DKNG', 'ABNB', 'QQQ', 'CRON', 'UAL', 'SIRI', 'CHPT', 'NKLA', 'JNJ', 'NKE', 'XOM', 'LUV', 'SOFI', 'INTC', 'ARKK', 'RIOT', 'MRO', 'PTON', 'DWAC', 'GOOG', 'OCGN', 'SHOP', 'JBLU', 'COST', 'ET', 'BNGO', 'RCL', 'SONY', 'TSM', 'WISH', 'RYCEY', 'V', 'JPM', 'CPRX', 
 'TGT', 'CLOV', 'ZM', 'FUBO', 'O', 'RITM', 'CRM', 'IVR', 'QS', 'SPHD', 'PENN')
 
-for x in ticks:
-      toStr = str(x)
-      syb = yf.Ticker(toStr)
-      data = pd.DataFrame(syb.history(interval="1m",period='1d',))
-      print(data['Open'].sum())
+def watchlist():
+    for x in ticks:
+        toStr = str(x)
+        syb = yf.Ticker(toStr)
+        data = pd.DataFrame(syb.history(interval="1m",period='1d',))
+        data2 = pd.DataFrame(syb.history(interval="2m",period='1d',))
+        if data['Open'].sum() < data2['Open'].sum():
+                    print(data['Open'].sum())
+                    print(data2['Open'].sum())
+                    print('Watch stock')
+        else:
+            print('Proceed to buy with robinhood')
+            
+    
+                    
 
-
-
-//create a function to check the current price every 5 minutes
+# print(datetime.datetime.now() - datetime.timedelta(minutes=5))
